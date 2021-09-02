1b84e3695d179f025e5ecb55f1703129cdae7579
JATT98 <31095919+JATT98@users.noreply.github.com>
2021-09-02 13:15:47 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import Menu from './Menu';
 import Categories from './Categories';
 import items from './data';
-const allCategories = ['all', ...new Set(items.map((item) => item.category))];
+const allCategories = ['Todos', ...new Set(items.map((item) => item.category))];
 
 function App() {
   const [menuItems, setMenuItems] = useState(items);
